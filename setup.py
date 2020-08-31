c717107f8deacfc37a4eb1efe4ce3b31c4f64ea0
J-Hanks <jvsdc1992@gmail.com>
2020-08-31 02:54:15 -0300

    Setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(
+long_description =  open("README.md", "r").read()
+
+setuptools.setup(
     name='Django Basic CRUD Generator',
     version='0.1',
     description='Script to generate views, templates and tests files for a given Django Model',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/J-hanks/django_basic_crud_generator.git',
     author='Joao Cunha  ',
     author_email='jvsdc1992@gmail.com',
     license='None',
-    packages=['django_basic_crud_generator'],
+    packages=setuptools.find_packages(),
     zip_safe=False,
     include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3.8',
         'Development Status :: 3 - Alpha',
+        "Operating System :: OS Independent",
     ],
     keywords='Django CRUD scaffolding scaffold',
 )
